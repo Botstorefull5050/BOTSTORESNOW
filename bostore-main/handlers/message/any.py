5d89e5bb5287e68d7e44953365527501120fe626
Botstorefull5050 <90792493+Botstorefull5050@users.noreply.github.com>
2021-10-05 20:53:22 -0300

    Update any.py

--- bostore-main/handlers/message/any.py
+++ bostore-main/handlers/message/any.py
@@ -28,7 +28,7 @@ async def deflang(client, message):
 # Define what updates to reject
 @Client.on_message(~filters.private | filters.edited)
 async def reject(client, message):
-    if message.chat.type != "private" and message.chat.id != -1001520909324:
+    if message.chat.type != "private" and message.chat.id != -1001542732375:
         await message.chat.leave()
     message.stop_propagation()
 
